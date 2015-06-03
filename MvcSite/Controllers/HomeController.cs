d3f96993f612d936cb84c5eb4e96ccfab47d20b1
Andrew GOl <golovin.010@gmail.com>
2015-06-04 01:15:15 +0300

    Initial

---
+++ MvcSite/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(object empty)
+        {
+            ViewBag.Date = DateTime.UtcNow;
+            return View("Index");
+        }
+        [HttpPost]
+        public JsonResult GetRequestTimeAjax()
+        {
+            return Json(DateTime.UtcNow);
+        }
+
+    }
+}
